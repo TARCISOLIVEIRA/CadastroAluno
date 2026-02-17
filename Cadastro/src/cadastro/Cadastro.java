f90c557834a4b7cd5ed18bbceaa6202cb424f357
Tarciso dos Santos de Oliveira <tarcisoliveira28@gmail.com>
2026-02-17 00:46:04 -0300

    salvando alteracao

--- Cadastro/src/cadastro/Cadastro.java
+++ Cadastro/src/cadastro/Cadastro.java
@@ -1,10 +1,34 @@
 
 package cadastro;
 
+import java.util.Scanner;
+
 
 public class Cadastro {
 
     public static void main(String[] args) {
+        int opc;
+        Scanner en = new Scanner(System.in);
+         
+        do{
+            System.out.println("----menu-----");
+            System.out.println("1 - inserir");
+            System.out.println("2 - listar");
+            System.out.println("3 - situação");
+            System.out.println("4 - sair");
+            
+            
+            System.out.println("Digite um codigo....");
+            opc = en.nextInt();
+            
+            switch(opc){
+                case 1:
+                    
+                    break; 
+            }
+                 
+            
+        }while(opc!=0);
         
         
     }
